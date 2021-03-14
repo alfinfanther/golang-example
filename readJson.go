3e1d02251c5da26663d3a4c22dc441664019046d
alfin <alfininfo@gmail.com>
2021-03-14 21:22:56 +0700

    readjson

---
+++ readJson.go
@@ -0,0 +1,36 @@
+package main
+import (
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"os"
+)
+
+type ListData struct{
+	Id int `json:"id"`
+	PropertyName string `json:"property_name"`
+	Url string `json:"url"`
+}
+
+
+func ReadJson() []ListData{
+	var list_data []ListData
+	jsonFile, err := os.Open("test.json")
+	if err != nil {
+		fmt.Println(err)
+	}
+	defer jsonFile.Close()
+
+	byteValue, _ := ioutil.ReadAll(jsonFile)
+	json.Unmarshal(byteValue, &list_data)
+
+	for i:=0; i < len(list_data); i++ {
+		lst := ListData{
+			Id: list_data[i].Id,
+			PropertyName: list_data[i].PropertyName,
+			Url: list_data[i].Url,
+		}
+		list_data = append(list_data,lst)
+	}
+	return list_data
+}
